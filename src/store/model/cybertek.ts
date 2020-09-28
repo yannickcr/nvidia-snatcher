d0d0f5ad5cd9f09dbb4512615090c4a6806da89e
Yannick Croissant <yannick.croissant@gmail.com>
2020-09-28 10:37:24 +0200

    more

--- src/store/model/cybertek.ts
+++ src/store/model/cybertek.ts
@@ -16,9 +16,69 @@ export const Cybertek: Store = {
 		},
 		{
 			brand: 'asus',
+			model: 'tuf oc',
+			series: '3090',
+			url: 'https://www.cybertek.fr/carte-graphique/asus/50648.aspx'
+		},
+		{
+			brand: 'asus',
 			model: 'tuf',
 			series: '3080',
 			url: 'https://www.cybertek.fr/carte-graphique/asus/48306.aspx'
+		},
+		{
+			brand: 'msi',
+			model: 'ventus 3x',
+			series: '3080',
+			url: 'https://www.cybertek.fr/carte-graphique/msi/50028.aspx'
+		},
+		{
+			brand: 'pny',
+			model: 'xlr8',
+			series: '3080',
+			url: 'https://www.cybertek.fr/carte-graphique/pny/50044.aspx'
+		},
+		{
+			brand: 'gigabyte',
+			model: 'gaming oc',
+			series: '3080',
+			url: 'https://www.cybertek.fr/carte-graphique/gigabyte/50048.aspx'
+		},
+		{
+			brand: 'gigabyte',
+			model: 'eagle oc',
+			series: '3080',
+			url: 'https://www.cybertek.fr/carte-graphique/gigabyte/50049.aspx'
+		},
+		{
+			brand: 'zotac',
+			model: 'trinity',
+			series: '3080',
+			url: 'https://www.cybertek.fr/carte-graphique/zotac/50128.aspx'
+		},
+		{
+			brand: 'palit',
+			model: 'gamingpro',
+			series: '3080',
+			url: 'https://www.cybertek.fr/carte-graphique/palit/50552.aspx'
+		},
+		{
+			brand: 'gainward',
+			model: 'phoenix',
+			series: '3080',
+			url: 'https://www.cybertek.fr/carte-graphique/gainward/50554.aspx'
+		},
+		{
+			brand: 'zotac',
+			model: 'trinity',
+			series: '3080',
+			url: 'https://www.cybertek.fr/carte-graphique/zotac/50599.aspx'
+		},
+		{
+			brand: 'msi',
+			model: 'gaming x trio',
+			series: '3080',
+			url: 'https://www.cybertek.fr/carte-graphique/msi/50027.aspx'
 		},
 		{
 			brand: 'asus',
@@ -33,10 +93,76 @@ export const Cybertek: Store = {
 			url: 'https://www.cybertek.fr/carte-graphique/asus/50610.aspx'
 		},
 		{
+			brand: 'palit',
+			model: 'gamingpro oc',
+			series: '3080',
+			url: 'https://www.cybertek.fr/carte-graphique/palit/50622.aspx'
+		},
+		{
 			brand: 'asus',
 			model: 'tuf',
 			series: '3090',
 			url: 'https://www.cybertek.fr/carte-graphique/asus/48307.aspx'
+		},
+		{
+			brand: 'msi',
+			model: 'ventus 3x',
+			series: '3090',
+			url: 'https://www.cybertek.fr/carte-graphique/msi/50026.aspx'
+		},
+		{
+			brand: 'gigabyte',
+			model: 'gaming oc',
+			series: '3090',
+			url: 'https://www.cybertek.fr/carte-graphique/gigabyte/50046.aspx'
+		},
+		{
+			brand: 'gigabyte',
+			model: 'eagle oc',
+			series: '3090',
+			url: 'https://www.cybertek.fr/carte-graphique/gigabyte/50047.aspx'
+		},
+		{
+			brand: 'gainward',
+			model: 'phoenix',
+			series: '3090',
+			url: 'https://www.cybertek.fr/carte-graphique/gainward/50551.aspx'
+		},
+		{
+			brand: 'pny',
+			model: 'xlr8',
+			series: '3090',
+			url: 'https://www.cybertek.fr/carte-graphique/pny/50043.aspx'
+		},
+		{
+			brand: 'palit',
+			model: 'gamingpro',
+			series: '3090',
+			url: 'https://www.cybertek.fr/carte-graphique/palit/50550.aspx'
+		},
+		{
+			brand: 'msi',
+			model: 'gaming x trio',
+			series: '3090',
+			url: 'https://www.cybertek.fr/carte-graphique/msi/50025.aspx'
+		},
+		{
+			brand: 'zotac',
+			model: 'trinity',
+			series: '3090',
+			url: 'https://www.cybertek.fr/carte-graphique/zotac/50129.aspx'
+		},
+		{
+			brand: 'gainward',
+			model: 'phoenix gs',
+			series: '3090',
+			url: 'https://www.cybertek.fr/carte-graphique/gainward/50548.aspx'
+		},
+		{
+			brand: 'palit',
+			model: 'gamingpro oc',
+			series: '3090',
+			url: 'https://www.cybertek.fr/carte-graphique/palit/50549.aspx'
 		},
 		{
 			brand: 'asus',
